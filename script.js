b2e28a7294868c1f70758577fdaaaff43b7a11f4
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-05 14:48:55 +0200

    add const for km

---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+chiedere all'utente il numero di km che vuole percorrere e età del passeggero
+moltiplicare i km inseriti (variabile dei km) per 0.21 € per avere la somma totale del viaggio
+SE età dell' utente < 18  {
+    sconto del 20%
+} ALTRIMENTI SE età dell'utente > 65 {
+    sconto 40%
+}
+risultato stampato in pagina con solo 2 decimali
+*/
+
+const kmToTravel = parseInt(prompt('Quanti km vuoi percorrere?'));
